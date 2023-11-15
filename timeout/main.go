c14c4ce5b6f1123bea037ef3f2ee377a076561ae
YusufSert <yusufcansert123@gmail.com>
2023-11-15 12:40:25 +0200

    T

--- timeout/main.go
+++ timeout/main.go
@@ -6,32 +6,35 @@ import (
 	"time"
 )
 
+type Result struct {
+	Response string
+	Error    error
+}
 type SlowFunction func(string) (string, error)
 
 type WithContext func(context.Context, string) (string, error)
 
 func Timeout(f SlowFunction) WithContext {
-	return func(ctx context.Context, arg string) (string, error) {
-		chres := make(chan string)
-		cherr := make(chan error)
+	return func(ctx context.Context, args string) (string, error) {
+		chres := make(chan Result)
 
 		go func() {
-			res, err := f(arg)
-			chres <- res
-			cherr <- err
+			close(chres)
+			res, err := f(args)
+			chres <- Result{res, err}
 		}()
 
 		select {
 		case res := <-chres:
-			return res, <-cherr
+			return res.Response, res.Error
 		case <-ctx.Done():
 			return "", ctx.Err()
 		}
 	}
 }
 
-func TimeoutAfterTime(f SlowFunction) WithContext {
-	return func(ctx context.Context, arg string) (string, error) {
+func TimeoutAfterTime(f SlowFunction) func(arg string) (string, error) {
+	return func(arg string) (string, error) {
 		chres := make(chan string)
 		cherr := make(chan error)
 
